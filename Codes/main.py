f2be95e0aec42b9210fd686238025d5da241fb5b
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-11-01 23:09:10 +0900

    feat: csv파일 완전 대체 및 기타 버그 수정

--- Codes/main.py
+++ Codes/main.py
@@ -2,29 +2,15 @@ import discord
 from discord.ext import commands
 import pandas as pd
 import numpy
-import defines
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-channel = None
-idDic = {}
 channelCsv = pd.read_csv('ChannelList.csv', encoding='UTF-8')
 reactionMsgCsv = pd.read_csv('ReactionMsgLists.csv', encoding='UTF-8')
 keiActivity = discord.Game('점검')
 
-
-@bot.event
-async def on_ready():
-    print('케이짱 등장')
-    await bot.change_presence(status=discord.Status.do_not_disturb, activity=keiActivity)
-    #await init()
-
-
-@bot.command(name="안녕")
-async def hello(ctx):
-    await ctx.send('안녕하세요')
 
 async def command_param_count(ctx, len, min, max):
     if (len < min):
@@ -34,6 +20,18 @@ async def command_param_count(ctx, len, min, max):
         await ctx.send(f'에러. 명령어 인자가 너무 많습니다.')
         return 1
     return 0
+
+
+@bot.event
+async def on_ready():
+    print('케이짱 등장')
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=keiActivity)
+    await init()
+
+
+@bot.command(name="안녕")
+async def hello(ctx):
+    await ctx.send('안녕하세요')
 
 
 @bot.command(name='역할자판기-채널등록')
@@ -49,7 +47,7 @@ async def register_role_channel(ctx, *args):
         await ctx.send(f'{args[0]}: 해당 채널은 이미 역할자판기 채널로 등록되어있습니다.')
         return
     channelCsv = pd.concat([channelCsv, pd.DataFrame({'guild_id' : [ctx.guild.id], 'channel_id' : [args[0][2:-1]]})])
-    channelCsv.to_csv('ChannelList.csv', mode='w')
+    channelCsv.to_csv('ChannelList.csv', mode='w', index=None)
     await ctx.send(f'{args[0]}: 해당 채널을 역할자판기 채널로 등록했습니다.')
 
 
@@ -67,57 +65,60 @@ async def add_role(ctx, *args):
     if (channelId == None):
         await ctx.send(f'본 서버에 등록된 역할자판기용 채널이 하나도 존재하지 않습니다. `/역할자판기-채널등록` 명령어를 사용해서 채널 등록을 먼저 진행해주세요.')
         return
-    messageId = args[0].split('/')[-1]
-    roleId = args[1][3:-1]
-    if (reactionMsgCsv[reactionMsgCsv['message_id']==numpy.int64(messageId)]['message_id'].values.size > 0):
+    
+    inputMsgId = args[0].split('/')[-1]
+    inputRoleId = args[1][3:-1]
+    if (reactionMsgCsv[reactionMsgCsv['message_id']==numpy.int64(inputMsgId)]['message_id'].values.size > 0):
         await ctx.send(f'{args[0]}: 해당 메세지는 이미 역할자판기 메세지로 등록되어있습니다.')
         return
-    if (reactionMsgCsv[reactionMsgCsv['role_id']==numpy.int64(roleId)]['role_id'].values.size > 0):
+    if (reactionMsgCsv[reactionMsgCsv['role_id']==numpy.int64(inputRoleId)]['role_id'].values.size > 0):
         await ctx.send(f'{args[1]}: 해당 역할은 이미 역할자판기에 등록되어있습니다.')
         return
     
-    reactionMsgCsv = pd.concat([reactionMsgCsv, pd.DataFrame({'message_id' : [messageId], 'role_id' : [roleId]})])
-    reactionMsgCsv.to_csv('ReactionMsgLists.csv', mode='w')
+    print(reactionMsgCsv)
+    pd.concat([reactionMsgCsv, pd.DataFrame({'channel_id' : [channelId], 'message_id' : [inputMsgId], 'role_id' : [inputRoleId]})]).to_csv('ReactionMsgLists.csv', mode='w', index=None)
+    reactionMsgCsv = pd.read_csv('ReactionMsgLists.csv', encoding='UTF-8')
+    print(reactionMsgCsv)
+    guild = bot.get_guild(guildId)
+    channel = guild.get_channel(channelId)
+    await channel.get_partial_message(inputMsgId).add_reaction('✅')
+    await channel.get_partial_message(inputMsgId).add_reaction('❌')
     await ctx.send(f'{args[0]} {args[1]}: 해당 메세지와 역할을 역할자판기에 등록했습니다.')
 
-
+# 이모지 반응 역할 부여 메소드
 @bot.event
 async def on_raw_reaction_add(ctx):
     if ctx.member.bot:
         return
-    if idDic[ctx.message_id] == None:
-        return
+    
     guild = bot.get_guild(ctx.guild_id)
+    channelId = channelCsv[channelCsv['guild_id']==numpy.int64(ctx.guild_id)]['channel_id'].values
+    messageId = reactionMsgCsv[reactionMsgCsv['message_id']==numpy.int64(ctx.message_id)]['message_id'].values
+    if (channelId.size == 0):
+        print('channel.size == 0')
+        return
+    if (messageId.size == 0):
+        print('messageId.size == 0')
+        return
+    
+    channel = guild.get_channel(channelId[0])
+    role = guild.get_role(reactionMsgCsv[reactionMsgCsv['message_id']==numpy.int64(ctx.message_id)]['role_id'].values[0])
     if str(ctx.emoji.name) == '✅':
         await channel.get_partial_message(ctx.message_id).remove_reaction('✅', ctx.member)
-        await ctx.member.add_roles(guild.get_role(idDic[ctx.message_id]))
+        await ctx.member.add_roles(role)
     elif str(ctx.emoji.name) == '❌':
         await channel.get_partial_message(ctx.message_id).remove_reaction('❌', ctx.member)
-        await ctx.member.remove_roles(guild.get_role(idDic[ctx.message_id]))
+        await ctx.member.remove_roles(role)
 
 
 async def init():
-    global guild, channel, msgId_list, idDic
-    guild = bot.get_guild(defines.guildId)
-    channel = guild.get_channel(defines.channelId)
-
-    idDic[defines.msgId_c] = defines.role_c
-    idDic[defines.msgId_cpp] = defines.role_cpp
-    idDic[defines.msgId_csharp] = defines.role_csharp
-    idDic[defines.msgId_java] = defines.role_java
-    idDic[defines.msgId_js] = defines.role_js
-    idDic[defines.msgId_python] = defines.role_python
-    await channel.get_partial_message(defines.msgId_c).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_c).add_reaction('❌')
-    await channel.get_partial_message(defines.msgId_cpp).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_cpp).add_reaction('❌')
-    await channel.get_partial_message(defines.msgId_csharp).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_csharp).add_reaction('❌')
-    await channel.get_partial_message(defines.msgId_java).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_java).add_reaction('❌')
-    await channel.get_partial_message(defines.msgId_js).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_js).add_reaction('❌')
-    await channel.get_partial_message(defines.msgId_python).add_reaction('✅')
-    await channel.get_partial_message(defines.msgId_python).add_reaction('❌')
+    channelIds = channelCsv['channel_id'].values
+    for i in range(channelIds.size):
+        guild = bot.get_guild(channelCsv[channelCsv['channel_id']==numpy.int64(channelIds[i])]['guild_id'].values[0])
+        channel = guild.get_channel(channelIds[i])
+        msgIds = reactionMsgCsv[reactionMsgCsv['channel_id']==numpy.int64(channelIds[i])]['message_id'].values
+        for j in range(msgIds.size):
+            await channel.get_partial_message(msgIds[i]).add_reaction('✅')
+            await channel.get_partial_message(msgIds[i]).add_reaction('❌')
 
 bot.run(open('TOKEN.txt', "r").read())
